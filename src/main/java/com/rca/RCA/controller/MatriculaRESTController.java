f657fdd206b0bb9090ccdb95be9006444eb995fe
LuisMC21 <104784109+LuisMC21@users.noreply.github.com>
2023-08-12 12:01:41 -0500

    Update MatriculaRESTController.java

--- src/main/java/com/rca/RCA/controller/MatriculaRESTController.java
+++ src/main/java/com/rca/RCA/controller/MatriculaRESTController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://profound-dodol-bcf620.netlify.app")
 @RestController
 @RequestMapping("/matricula")
 public class MatriculaRESTController {
